3a546e0324ce54816452cceeba37c6993aeb13f0
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2024-02-13 09:11:55 -0500

    Basic project complete

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+const messages = [
+    {
+        text: "Is anyone there?",
+        user: "Trevor",
+        added: new Date()
+    },
+    {
+        text: "Humans create love",
+        user: "serenee",
+        added: new Date()
+    }
+];
+
 // GET home page
 
 router.get('/', (req, res, next) => {
-    const messages = [
-        {
-            text: "Is anyone there?",
-            user: "Trevor",
-            added: new Date()
-        },
-        {
-            text: "Humans create love",
-            user: "serenee",
-            added: new Date()
-        }
-    ];
     res.render('index', { title: "Mini Messageboard", messages: messages })
 })
 
 router.get('/new', (req, res, next) => {
     res.render('form', { title: "New Message" })
+})
+
+// new message
+
+router.post('/new', (req, res, next) => {
+    const messageText = req.body.message;
+    const messageAuthor = req.body.author;
+    messages.push({text: messageText, user: messageAuthor, added: new Date()});
+
+    res.redirect('/');
 })
 
 module.exports = router;
